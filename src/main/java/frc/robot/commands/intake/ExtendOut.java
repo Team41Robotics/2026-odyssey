a079432262f2f54d2030b9133983dbcf65c0135c
Jason Zeng <jasonzeng124@gmail.com>
2026-03-29 17:55:18 -0400

    who won????

--- src/main/java/frc/robot/commands/intake/ExtendOut.java
+++ src/main/java/frc/robot/commands/intake/ExtendOut.java
@@ -14,11 +14,13 @@ public class ExtendOut extends Command {
 	@Override
 	public void execute() {
 		intake.targetExtendVoltage = IntakeHW.EXTEND_OUT_VOLTAGE;
+		intake.targetIntakeVoltage = IntakeHW.INTAKE_REVERSE_VOLTAGE;
 	}
 
 	@Override
 	public void end(boolean interrupted) {
 		intake.targetExtendVoltage = 0;
+		intake.targetIntakeVoltage = IntakeHW.INTAKE_VOLTAGE;
 	}
 
 	@Override
